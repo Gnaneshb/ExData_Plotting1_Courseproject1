2c28b00016465eca917f21396f58bdc3928183be
Gnaneshb <gnanesh.bammidi320@gmail.com>
2016-09-12 11:40:22 +0530

    adding plot4

--- plot4.R
+++ plot4.R
@@ -10,16 +10,18 @@ rm(EPC)
 EPC_suset_convert_data <- paste(as.Date(EPC_subset$Date), EPC_subset$Time)
 EPC_subset$Datetime <- as.POSIXct(EPC_suset_convert_data)
 
-## Plot 3
+## Plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(EPC_subset, {
-  plot(Sub_metering_1~Datetime, type="l",
-       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Global_active_power~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~Datetime, type="l", ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
+  
 ## Saving to file
-dev.copy(png, file="C:/Users/GNANESH BAMMIDI/Desktop/Courseprojects/ExData_Plotting1/Project1/plot3.png", height=480, width=480)
+dev.copy(png, file="C:/Users/GNANESH BAMMIDI/Desktop/Courseprojects/ExData_Plotting1/Project1/plot4.png", height=480, width=480)
 dev.off()
